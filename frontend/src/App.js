9ef7aa1f62681616f418d92ea367f121781c36fb
BraanDaan <branmitchell12@gmail.com>
2023-05-03 09:54:38 -0700

    bootstrap.min.css was moved to src root

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.min.css';
 import HomeScreen from "./screens/home.js";
 
 function App() {
